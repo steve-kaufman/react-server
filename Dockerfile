dee7ec5722daca755913d4ae3cf7698e67fd6e43
Steven <steveplaysguiola@gmail.com>
2021-03-30 17:47:25 -0500

    blah

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,18 @@ FROM golang:alpine as builder
 
 WORKDIR /go/src/server
 
+COPY go.mod .
 COPY main.go .
 
-RUN go build -o server
+RUN GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o serve
 
 
-FROM alpine:latest
+FROM scratch
 
-ENV PATH=$PATH:/bin
+COPY --from=builder /lib/ld-musl-x86_64.so.1 /lib/
 
-COPY --from=builder /go/src/server/server /bin
+COPY --from=builder /go/src/server/serve /bin/
+
+ENV PATH=/bin
 
 EXPOSE 8080
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var buildPath string
 
 func main() {
-	buildPath = os.Getenv("REACT_BUILD_PATH")
+	buildPath = os.Args[1]
 	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(serveSpa)))
 }
 
@@ -18,15 +18,23 @@ func serveSpa(w http.ResponseWriter, r *http.Request) {
 	serveFileOrIndex(w, r, getFilepath(r.URL.Path))
 }
 
-func getFilepath(path string) string {
-	return filepath.Join(buildPath, path)
-}
-
-func serveFileOrIndex(w http.ResponseWriter, r *http.Request, filepath string) {
-	if _, err := os.Stat(filepath); err != nil {
+func serveFileOrIndex(w http.ResponseWriter, r *http.Request, requestedFilepath string) {
+	if !fileExists(requestedFilepath) {
 		http.ServeFile(w, r, getFilepath("index.html"))
 		return
 	}
 
-	http.ServeFile(w, r, filepath)
+	http.ServeFile(w, r, requestedFilepath)
+}
+
+func fileExists(requestedFilepath string) bool {
+	_, err := os.Stat(requestedFilepath)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func getFilepath(path string) string {
+	return filepath.Join(buildPath, path)
 }
